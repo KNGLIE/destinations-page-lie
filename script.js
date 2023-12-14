c9d37041a99f9e64f0ab991ccdf996001f8cae64
Lance Evans <lance.evans@tlgcohort.com>
2023-12-14 15:17:45 -0700

    added create card funcitonality

--- script.js
+++ script.js
@@ -12,5 +12,25 @@ user_input_form.addEventListener("submit", (event) => {
   const descr = description.value;
 
   user_input_form.reset()
-  console.log(destinationName, locName, photoUrl, descr);
+  const card = createCard({destinationName, locName, photoUrl, descr});
+
+  cards_container.appendChild(card);
 });
+
+function createCard({destinationName, locName, photoUrl, descr}){
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("style", "width: 18rem;");
+    card.innerHTML = `
+    <img src="${photoUrl}" class="card-img-top" alt="${destinationName} at ${locName}">
+    <div class="card-body">
+        <h5 class="card-title">${destinationName}</h5>
+        <p class="card-text">${locName}</p>
+        ${descr ? `<p class="card-text">${descr}</p>` : ''}
+        <button type="button" class="btn btn-info">Edit</button>
+        <button type="button" class="btn btn-danger">Delete</button>
+    </div>
+`;
+
+    return card;
+}
